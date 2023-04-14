9d344085fa8895baf3ab24f42f530d4641549d5b
DevButani <devbutani26@gmail.com>
2023-04-14 18:51:18 +0530

    Updates

---
+++ Layout.js
@@ -0,0 +1,41 @@
+import { Outlet, Link } from "react-router-dom";
+import './Layout.css';
+import logo from './logo.jpg';
+import React from 'react';
+
+const Layout = () => {
+    const [logotext, setlogotext] = React.useState(true)
+    const text1="Hi There!"
+    const text2="Welcome to Diligent Study Group!"
+    function clickhandler() {
+    setlogotext(!logotext)
+    }
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logobox">
+          <img onClick={clickhandler} className="logo" src={logo} alt="logo"/>
+          <div className="logotextbox"><p>{logotext? text1:text2}</p></div>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
